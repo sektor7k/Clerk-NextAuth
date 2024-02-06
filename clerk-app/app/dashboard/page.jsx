5f8bc3970828d801b8554f3ee224944179d9658c
omer aydin <omeraydin2112@gmail.com>
2024-02-07 00:34:35 +0300

    dashboard page

---
+++ clerk-app/app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <>
+    <h1 className='mb-5 text-2xl font-bold'>Dashboard</h1>
+    <p className='mb-5 '>Welcome to the Dashboard</p>
+    </>
+  )
+}
+
+export default DashboardPage
