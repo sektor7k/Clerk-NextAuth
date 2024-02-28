785571d9d22972157a3f01631c890c8d5bf728b0
sektor7k <omeraydin2112@gmail.com>
2024-02-28 16:55:42 +0300

    create dashboard page

--- auth-tutorial/app/page.tsx
+++ auth-tutorial/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <div className=" text-red-400">
-      hello world
-    </div>
+    <Button variant={"custom"} size={"lg"}>
+      Click Me
+    </Button>
   );
 }
