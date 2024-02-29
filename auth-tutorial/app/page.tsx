611d479a5cded0d85248896f899adfd5f3bde2ba
sektor7k <omeraydin2112@gmail.com>
2024-02-29 17:11:25 +0300

    global css modified

--- auth-tutorial/app/page.tsx
+++ auth-tutorial/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
+import { useEffect } from "react";
 
 export default function Home() {
+
   return (
-    <Button variant={"custom"} size={"lg"}>
+    <Button size={"lg"}>
       Click Me
     </Button>
   );
