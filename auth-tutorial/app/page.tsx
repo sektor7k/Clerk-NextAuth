235da05fc918f9c08f58107fe69f375e823715df
sektor7k <omeraydin2112@gmail.com>
2024-02-29 19:24:19 +0300

    main page modified

--- auth-tutorial/app/page.tsx
+++ auth-tutorial/app/page.tsx
@@ -1,11 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
 
 export default function Home() {
 
+  // const font = Poppins({ 
+  //   subsets: ["latin"],
+  //   weight: ["600"]
+  // });
+
   return (
-    <Button size={"lg"}>
-      Click Me
-    </Button>
+    <main className=" flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className=" space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md")}>
+         🔐 Auth
+        </h1>
+        <p className=" text-white text-lg">
+          A simple authentication service
+        </p>
+        <div>
+          <LoginButton mode="redirect">
+          <Button variant={"secondary"} size={"lg"} >
+            Sign in
+          </Button>
+          </LoginButton>
+        </div>
+      </div>
+      
+    </main>
   );
 }
