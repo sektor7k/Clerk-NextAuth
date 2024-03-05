e2fce9eb61d5ca11b19bd9708df4b6425106f02a
sektor7k <omeraydin2112@gmail.com>
2024-03-05 14:34:02 +0300

    user register actions

--- auth-tutorial/actions/register.ts
+++ auth-tutorial/actions/register.ts
@@ -1,7 +1,9 @@
 "use server"
 
 import * as z from "zod";
-import { LoginSchema, RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +12,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return {error: "Invalid fields!"};
     }
 
-    return { success: "Email sent!"}
+    const {email, password, name} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (existingUser) {
+        return { error: 'Email is already in use!' };
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password: hashedPassword
+        },
+    });
+
+    // TODO: Send verification token email
+
+    return { success: "User created"}
 }
