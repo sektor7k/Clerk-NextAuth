ecd34f3de501b2b7dd52d03b72698f14d665ffe5
sektor7k <omeraydin2112@gmail.com>
2024-03-05 14:44:50 +0300

    get user by id

--- auth-tutorial/actions/register.ts
+++ auth-tutorial/actions/register.ts
@@ -4,6 +4,7 @@ import * as z from "zod";
 import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -15,11 +16,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const {email, password, name} = validatedFields.data;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const existingUser = await db.user.findUnique({
-        where: {
-            email
-        }
-    })
+    const existingUser = await getUserByEmail(email);
 
     if (existingUser) {
         return { error: 'Email is already in use!' };
