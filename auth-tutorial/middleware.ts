b7c1cbd242b95d64ab0a43feebfb865070536bf6
sektor7k <omeraydin2112@gmail.com>
2024-03-06 06:04:10 +0300

    middleware complated

--- auth-tutorial/middleware.ts
+++ auth-tutorial/middleware.ts
@@ -1,12 +1,42 @@
-import { auth } from "./auth"
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+import {
+    DEFAULT_LOGIN_REDİRECT,
+    apiAuthPrefix,
+    authRoutes,
+    publicRoutes
+} from "@/routes";
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+    const {nextUrl} = req;
     const isLoggedIn = !!req.auth;
-    console.log("ROUTE: ", req.nextUrl.pathname);
-    console.log("IS LOGGEDIN: ", isLoggedIn);
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) {
+        return null;
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDİRECT, nextUrl))
+        }
+        return null;
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    return null;
+
 })
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
+
