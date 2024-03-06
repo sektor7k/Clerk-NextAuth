f1f1607850a37161d4db46378d74225736d77205
sektor7k <omeraydin2112@gmail.com>
2024-03-06 06:04:46 +0300

    login add signin

--- auth-tutorial/actions/login.ts
+++ auth-tutorial/actions/login.ts
@@ -1,14 +1,39 @@
 "use server"
 
 import * as z from "zod";
+import { AuthError } from "next-auth";
+
+import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
+import { DEFAULT_LOGIN_REDİRECT } from "@/routes";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
 
     if (!validatedFields.success) {
-        return {error: "Invalid fields!"};
+        return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!"}
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDİRECT
+        });
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials!" }
+                default:
+                    return { error: "Something went wrong!" };
+
+            }
+        }
+
+        throw error;
+    }
+
 }
